bf1924e04ca9566a48f7ef15638e772f41204094
sa <sixsecondfiesta@gmail.com>
2016-11-14 00:43:35 -0500

    report if no results for image search; fall back to inexact first

--- dist/commands/images.js
+++ dist/commands/images.js
@@ -4,19 +4,18 @@ const got = require('got');
 const cheerio = require('cheerio');
 const util_1 = require('../util/util');
 // based on https://github.com/jimkang/g-i-s/blob/master/index.js
-const makeSearch = (animated) => (term) => got('http://images.google.com/search', {
+const requestAndParse = (term, animated, exact) => got('http://images.google.com/search', {
     query: {
         q: term,
         tbm: 'isch',
-        nfpr: 1,
+        nfpr: exact ? 1 : 0,
         tbs: animated ? 'itp:animated' : undefined
     },
     timeout: 5000,
     headers: {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
     }
-})
-    .then(res => {
+}).then(res => {
     const $ = cheerio.load(res.body);
     const metaLinks = $('.rg_meta');
     const urls = [];
@@ -28,9 +27,21 @@ const makeSearch = (animated) => (term) => got('http://images.google.com/search'
             }
         }
     });
-    return util_1.randomInArray(urls.slice(0, 5));
+    return urls;
 });
-const imageSearch = makeSearch(false);
+const search = (term, animated = false) => requestAndParse(term, animated, true).then(res => {
+    if (res.length === 0) {
+        //if no results, try an inexact search
+        return requestAndParse(term, animated, false);
+    }
+    return res;
+}).then(res => {
+    if (res.length === 0) {
+        //if no results, try an inexact search
+        return 'nothing :(';
+    }
+    return util_1.randomInArray(res.slice(0, 5));
+});
 exports.imageSearchCommand = chatter_1.createCommand({
     name: 'image',
     aliases: [`what's a`, `what's`, `who's`, 'show me'],
@@ -39,9 +50,8 @@ exports.imageSearchCommand = chatter_1.createCommand({
     if (message.length === 0) {
         return false;
     }
-    return imageSearch(message);
+    return search(message);
 });
-const gifSearch = makeSearch(true);
 exports.gifSearchCommand = chatter_1.createCommand({
     name: 'gifsearch',
     aliases: ['gif me the', 'gif me a', 'gif me', 'gif'],
@@ -50,5 +60,5 @@ exports.gifSearchCommand = chatter_1.createCommand({
     if (message.length === 0) {
         return false;
     }
-    return gifSearch(message);
+    return search(message, true);
 });
--- src/commands/images.ts
+++ src/commands/images.ts
@@ -6,19 +6,18 @@ import { randomInArray } from '../util/util'
 
 // based on https://github.com/jimkang/g-i-s/blob/master/index.js
 
-const makeSearch = (animated : boolean) => (term : string) => got('http://images.google.com/search', {
+const requestAndParse = (term : string, animated : boolean, exact : boolean) => got('http://images.google.com/search', {
   query: {
     q: term,
     tbm: 'isch', // perform an image search
-    nfpr: 1, //exact search, don't correct typos
+    nfpr: exact ? 1 : 0, //exact search, don't correct typos
     tbs: animated ? 'itp:animated' : undefined
   },
   timeout: 5000,
   headers: {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
   }
-})
-.then(res => {
+}).then(res => {
   const $ = cheerio.load(res.body)
   const metaLinks = $('.rg_meta')
   const urls : string[] = []
@@ -31,10 +30,23 @@ const makeSearch = (animated : boolean) => (term : string) => got('http://images
       // Elements without metadata.ou are subcategory headings in the results page.
     }
   })
-  return randomInArray(urls.slice(0, 5))
+  return urls
 })
 
-const imageSearch = makeSearch(false)
+const search = (term : string, animated = false) => requestAndParse(term, animated, true).then(res => {
+  if(res.length === 0) {
+    //if no results, try an inexact search
+    return requestAndParse(term, animated, false)
+  }
+  return res
+}).then(res => {
+  if(res.length === 0) {
+    //if no results, try an inexact search
+    return 'nothing :('
+  }
+  return randomInArray(res.slice(0, 5))
+})
+
 export const imageSearchCommand = createCommand(
   {
     name: 'image',
@@ -45,12 +57,10 @@ export const imageSearchCommand = createCommand(
     if(message.length === 0) {
       return false
     }
-    return imageSearch(message)
+    return search(message)
   }
 )
 
-
-const gifSearch = makeSearch(true)
 export const gifSearchCommand = createCommand(
   {
     name: 'gifsearch',
@@ -61,6 +71,6 @@ export const gifSearchCommand = createCommand(
     if(message.length === 0) {
       return false
     }
-    return gifSearch(message)
+    return search(message, true)
   }
 )

26747b6a93d9bac25eba9c92ad2c7462aa3d2152
sa <sixsecondfiesta@gmail.com>
2017-02-17 00:40:15 -0500

    update package.json and style tweak in .ts file

--- src/actions/markov.ts
+++ src/actions/markov.ts
@@ -3,7 +3,7 @@ interface AddSentenceAction {
   sentence : string
 }
 
-export const addSentenceAction = (sentence: string) : AddSentenceAction => ({ type : 'ADD_SENTENCE', sentence })
+export const addSentenceAction = (sentence : string) : AddSentenceAction => ({ type : 'ADD_SENTENCE', sentence })
 
 export function isAddSentenceAction(action : { type : string }) : action is AddSentenceAction {
   return action.type === 'ADD_SENTENCE'

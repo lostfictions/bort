39ee7fe59adaf34fe0d4ea1bcac4d435b5a44747
sa <lostfictions@users.noreply.github.com>
2017-11-25 06:50:13 -0500

    experimentally remove build artifacts

--- dist/actions/markov.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addSentenceAction = (sentence) => ({ type: 'ADD_SENTENCE', sentence });
-function isAddSentenceAction(action) {
-    return action.type === 'ADD_SENTENCE';
-}
-exports.isAddSentenceAction = isAddSentenceAction;
-//# sourceMappingURL=markov.js.map

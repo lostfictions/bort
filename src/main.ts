3332b56f3e3530ceaedd1039446ed59fdf82e8d0
lostfictions <lostfictions@users.noreply.github.com>
2018-08-10 02:36:22 -0400

    disable server component

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ require("source-map-support").install();
 
 import { makeDiscordBot } from "./clients/discord";
 import { makeCLIBot } from "./clients/cli";
-import { createServer } from "./components/server";
-import { USE_CLI, PORT, DISCORD_TOKEN } from "./env";
+// import { createServer } from "./components/server";
+import {
+  USE_CLI,
+  // PORT,
+  DISCORD_TOKEN
+} from "./env";
 
 if (USE_CLI) {
   makeCLIBot();
 } else {
-  createServer(PORT);
-
+  // createServer(PORT);
   if (DISCORD_TOKEN.length > 0) {
     const discordBot = makeDiscordBot(DISCORD_TOKEN);
     discordBot.login();

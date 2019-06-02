674333f84a6149418a89e46fd00a0ec5b900a1ec
lostfictions <lostfictions@users.noreply.github.com>
2019-06-02 04:39:25 -0400

    choose command ignores empty messages

--- src/commands/choose.ts
+++ src/commands/choose.ts
@@ -9,6 +9,7 @@ export default makeCommand(
     description: "pick an option at random from a comma-separated list"
   },
   async ({ message: rawMessage, store }) => {
+    if (rawMessage.trim().length === 0) return false;
     const { message, prefix } = await maybeTraced(rawMessage, store);
     return prefix + randomInArray(message.split(",").map(s => s.trim()));
   }

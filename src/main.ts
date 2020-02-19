ff081d219f753035f0babacc9ebe509d820b0932
lostfictions <lostfictions@users.noreply.github.com>
2020-02-19 06:03:24 -0500

    hackishly fix stack traces under ts-node

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-require("source-map-support").install();
-
 import { makeDiscordBot } from "./clients/discord";
 import { makeCLIBot } from "./clients/cli";
 import { USE_CLI, DISCORD_TOKEN } from "./env";
 
 if (USE_CLI) {
   makeCLIBot();
-} else if (DISCORD_TOKEN.length > 0) {
-  const discordBot = makeDiscordBot(DISCORD_TOKEN);
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  discordBot.login();
+} else {
+  require("source-map-support").install();
+  if (DISCORD_TOKEN.length > 0) {
+    const discordBot = makeDiscordBot(DISCORD_TOKEN);
+    discordBot.login().catch(e => {
+      throw e;
+    });
+  }
 }

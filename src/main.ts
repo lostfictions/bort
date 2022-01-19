9d2486f1fa383cdd3055f05829dfd520cd3d3dff
lostfictions <lostfictions@users.noreply.github.com>
2022-01-19 00:28:48 -0500

    actually remove source-map-support

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,9 @@ import { USE_CLI, DISCORD_TOKEN } from "./env";
 
 if (USE_CLI) {
   makeCLIBot();
-} else {
-  require("source-map-support").install();
-  if (DISCORD_TOKEN.length > 0) {
-    const discordBot = makeDiscordBot(DISCORD_TOKEN);
-    discordBot.login().catch((e) => {
-      throw e;
-    });
-  }
+} else if (DISCORD_TOKEN.length > 0) {
+  const discordBot = makeDiscordBot(DISCORD_TOKEN);
+  discordBot.login().catch((e) => {
+    throw e;
+  });
 }
